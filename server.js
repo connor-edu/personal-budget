94a715a5b9dccf1f67a2279affcc1f3e40dc0bdf
Connor Love <connor@dotconnor.com>
2020-10-30 21:45:13 -0400

    update

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const path = require("path");
-const fs = require("fs").promises;
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/personal_budget", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+const budgetScheme = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  budget: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+    validate: /^#[0-9A-Fa-f]{6}$/,
+  },
+});
+
+const Budget = db.model("budget", budgetScheme, "budget");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/hello", (req, res) => {
   return res.send("Hello World!");
 });
 
-app.get("/budget", (req, res) => {
-  return fs.readFile(path.join(__dirname, "budget.json"), "utf-8").then((budget) => {
-    return res.contentType("application/json").send(budget);
+app.get("/budget", async (req, res) => {
+  const budget = await Budget.find();
+  return res.json(budget);
+});
+app.post("/budget", async (req, res) => {
+  const budget = new Budget({
+    title: req.body.title,
+    budget: req.body.budget,
+    color: req.body.color,
   });
+  await budget.save();
+  return res.json(budget);
 });
 
 app.listen(port, () => {
